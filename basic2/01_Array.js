d02da402c6c4db17cc63b381f1fc62fc9dd878c6
Anushka Chand <anushkakaushik0801@gmail.com>
2026-01-13 21:31:22 +0530

    Add initial array examples in 01_Array.js

---
+++ basic2/01_Array.js
@@ -0,0 +1,13 @@
+//  Array
+
+// the array, which is a collection of elements.
+// it is zero indexing
+// Shallow copy = share same copy
+
+
+// const myArray = [ 1,2,3,4]
+var myArray = [1, "Hello", [2, 3]];
+console.log(myArray[0]);
+
+const myHeroes = [ "IronMan", "Spiderman" ];
+console.log(myHeroes[1]);

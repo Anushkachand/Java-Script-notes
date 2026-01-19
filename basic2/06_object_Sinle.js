d1c2144adecb162c81391e217e75283e71dc95b9
Anushka Chand <anushkakaushik0801@gmail.com>
2026-01-19 23:38:27 +0530

    Delete basic2 directory

--- basic2/06_object_Sinle.js
+++
@@ -1,9 +0,0 @@
-const tinder_User = new Object() // singleten Oject
-
-const tinderUser = {} // non - singlet
-tinderUser.id = "123ab"
-tinderUser.isLogged = false
-
-console.log(tinderUser);
-
-const regularUser
